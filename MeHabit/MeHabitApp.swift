39eee925c1b6e3648dc9f56212d94fe5bf8495dc
leila <143362178+1e1104k4@users.noreply.github.com>
2024-01-11 12:40:32 -0600

    Initial Commit

---
+++ MeHabit/MeHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeHabitApp.swift
+//  MeHabit
+//
+//  Created by Leila on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MeHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
